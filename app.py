acc639ecec50ca9ea9230dbccaeaa7b5000a39e2
Dmytro Cheusov <d.cheusov@abona-erp.com>
2020-01-16 12:30:45 +0200

    Implement POST method

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -30,6 +30,30 @@ def get_task(task_id):
     if len(task) == 0:
         abort(410)
     return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(410)
+def not_found(error):
+    return make_response(jsonify({'error': 'Gone'}), 410)
 
 
 if __name__ == '__main__':
