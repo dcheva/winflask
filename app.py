514a3a6605644d2d792f26cc7a26f7bc1796c405
Dmytro Cheusov <d.cheusov@abona-erp.com>
2020-01-16 13:13:29 +0200

    Implement 400 Bad request

--- app.py
+++ app.py
@@ -46,9 +46,24 @@ def create_task():
     return jsonify({'task': task}), 201
 
 
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.errorhandler(401)
+def not_found(error):
+    return make_response(jsonify({'error': 'Unauthorized Error'}), 401)
+
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(405)
+def not_found(error):
+    return make_response(jsonify({'error': 'Method Not Allowed'}), 405)
 
 
 @app.errorhandler(410)
