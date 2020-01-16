9cce2e5bfb3e06cf4110537fb8383f9249ff648f
Dmytro Cheusov <d.cheusov@abona-erp.com>
2020-01-16 14:17:39 +0200

    Remove auth

--- app.py
+++ app.py
@@ -1,33 +1,8 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, request, make_response, url_for
-from flask_httpauth import HTTPBasicAuth
+from flask import Flask, jsonify, abort, make_response, request
+from pip._vendor.pyparsing import unicode
 
-app = Flask(__name__, static_url_path="")
-auth = HTTPBasicAuth()
-
-
-@auth.get_password
-def get_password(username):
-    if username == 'miguel':
-        return 'python'
-    return None
-
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
-    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
-
-
-@app.errorhandler(400)
-def not_found(error):
-    return make_response(jsonify({'error': 'Bad request'}), 400)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
+app = Flask(__name__)
 
 tasks = [
     {
@@ -45,33 +20,20 @@ tasks = [
 ]
 
 
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
-        else:
-            new_task[field] = task[field]
-    return new_task
-
-
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
-@auth.login_required
 def get_tasks():
-    return jsonify({'tasks': map(make_public_task, tasks)})
+    return jsonify({'tasks': tasks})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-@auth.login_required
 def get_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
+    task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
-        abort(404)
-    return jsonify({'task': make_public_task(task[0])})
+        abort(410)
+    return jsonify({'task': task[0]})
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
-@auth.login_required
 def create_task():
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -82,13 +44,12 @@ def create_task():
         'done': False
     }
     tasks.append(task)
-    return jsonify({'task': make_public_task(task)}), 201
+    return jsonify({'task': task}), 201
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
-@auth.login_required
 def update_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
+    task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     if not request.json:
@@ -102,17 +63,41 @@ def update_task(task_id):
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': make_public_task(task[0])})
+    return jsonify({'task': task[0]})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
-@auth.login_required
 def delete_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
+    task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
     return jsonify({'result': True})
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.errorhandler(403)
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(405)
+def not_allowed(error):
+    return make_response(jsonify({'error': 'Method Not Allowed'}), 405)
+
+
+@app.errorhandler(410)
+def gone(error):
+    return make_response(jsonify({'error': 'Gone'}), 410)
 
 
 if __name__ == '__main__':
